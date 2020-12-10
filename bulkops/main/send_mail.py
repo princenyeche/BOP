571d49f37cb93c90a8e4dc9d7589631358ea7d61
Prince <hakwumerunyeche@gmail.com>
2020-12-10 22:31:01 +0800

    update to v2.0.4

--- bulkops/main/send_mail.py
+++ bulkops/main/send_mail.py
@@ -1,23 +1,23 @@
 from flask import render_template
-from bulkops.main.message_notifier import send_message
+from bulkops.main.message_notifier import send_user_message
 from bulkops.email import send_message
 from bulkops import bulk
 
 
 def send_app_messages(admin, user, form):
-    send_message(f"[{bulk.config['APP_NAME']}] Requested Jobs Completed",
-                 sender=admin.id,
-                 recipients=user.id,
-                 text_body=render_template("email/app_notice_form.txt", user=user, form=form),
-                 notify=user)
+    send_user_message(f"[{bulk.config['APP_NAME']}] Requested Jobs Completed",
+                      sender=admin.id,
+                      recipients=user.id,
+                      text_body=render_template("email/app_notice_form.txt", user=user, form=form),
+                      notify=user)
 
 
 def send_error_messages(admin, user, form):
-    send_message(f"[{bulk.config['APP_NAME']}] Requested Jobs Failed",
-                 sender=admin.id,
-                 recipients=user.id,
-                 text_body=render_template("email/app_error_form.txt", user=user, form=form),
-                 notify=user)
+    send_user_message(f"[{bulk.config['APP_NAME']}] Requested Jobs Failed",
+                      sender=admin.id,
+                      recipients=user.id,
+                      text_body=render_template("email/app_error_form.txt", user=user, form=form),
+                      notify=user)
 
 
 def send_admin_message(admin, user, form):
