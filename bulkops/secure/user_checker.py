ae2d496880d83283fd9343ea8fcee57dbae05d40
Prince <hakwumerunyeche@gmail.com>
2021-06-10 20:23:26 +0200

    Create user_checker.py

---
+++ bulkops/secure/user_checker.py
@@ -0,0 +1,14 @@
+from flask_login import current_user
+from flask import redirect, url_for, flash
+from functools import wraps
+
+
+def validate_account(control):
+    @wraps(control)
+    def allow_accounts(*args, **kwargs):
+        if current_user.confirm_user is False or current_user.confirm_user is None:
+            flash("Please verify your account!", "warning")
+            return redirect(url_for("unconfirmed"))
+        return control(*args, **kwargs)
+
+    return allow_accounts
