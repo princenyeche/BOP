c65863c3058f8d74a083c071ad4ef8b49780c8a6
Prince <hakwumerunyeche@gmail.com>
2021-10-15 17:23:35 +0200

    patch

--- bulkops/secure/views.py
+++ bulkops/secure/views.py
@@ -112,7 +112,7 @@ def signup():
                 flash(f"Your URL \"{y}\" is not the expected value. Do you mean {y.lstrip(string.punctuation)} instead?")
         elif y.endswith("atlassian.net") or y.endswith("jira-dev.com") \
                 or y.endswith("jira.com"):
-            pattern = r"[^\w\d||\.||-]"
+            pattern = r"[^\w\d\|\|\.\|\|-]"
             sanity_url = re.findall(pattern, y)
             if len(sanity_url) > 0:
                 # The URL must contain an invalid character here, so we don't accept it.
