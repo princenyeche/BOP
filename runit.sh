acaa023aea739ff356048c9784087397f9e83369
Prince <hakwumerunyeche@gmail.com>
2022-01-02 02:35:26 +0100

    added workers

--- runit.sh
+++ runit.sh
@@ -4,4 +4,5 @@ gunicorn startup:bulk \
          --bind=0.0.0.0:5000 \
          --timeout 60 \
          --graceful-timeout 60 \
-         --keep-alive 120
+         --keep-alive 120 \
+         --workers 2
