fa39119289fed22d90ba3e8098c9bc853c5321c5
Prince <hakwumerunyeche@gmail.com>
2022-12-14 02:29:25 +0100

    Added a name to the sender

--- bulkops/email.py
+++ bulkops/email.py
@@ -10,7 +10,7 @@ def send_async_email(bulk, msg):
 
 
 def send_message(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
+    msg = Message(subject, sender=(bulk.config['APP_NAME_SINGLE'], sender), recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     Thread(target=send_async_email, args=(bulk, msg)).start()
